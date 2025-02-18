a8c0a8bbd42574841e7824a850fee09c93d5fe21
Tuttee <111608871+Tuttee@users.noreply.github.com>
2025-02-18 19:17:56 +0200

    First commit. Create project

---
+++ src/test/java/com/CactiEncyclopedia/CactiEncyclopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CactiEncyclopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CactiEncyclopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
